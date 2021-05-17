70781466022e3092f28aeabdb25e0018054d18fe
sandrapeterr <84057680+sandrapeterr@users.noreply.github.com>
2021-05-17 19:20:39 +0530

    created new class

---
+++ src/test/java/test/demo1.java
@@ -0,0 +1,17 @@
+package test;
+
+public class demo1 {
+
+public static void main(String[] args ) {
+		
+		System.out.println("Hi github");
+		
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+			System.out.println("/n");
+			System.out.println("/n");
+			System.out.println("/n");
+		}
+	}
+}
