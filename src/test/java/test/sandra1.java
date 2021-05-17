2fdcd8c5b53e76d92fd636b49339f2d1d0630fdf
sandrapeterr <84057680+sandrapeterr@users.noreply.github.com>
2021-05-17 19:05:37 +0530

    add codes

--- src/test/java/test/sandra1.java
+++ src/test/java/test/sandra1.java
@@ -5,6 +5,11 @@ public class sandra1 {
 	public static void main(String[] args ) {
 		
 		System.out.println("Hi github");
+		
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
 	}
 
 }
